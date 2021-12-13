f7a55cb3aab96195a24bcbddec198e8e4b5d14df
BillyYank <taras4834@gmail.com>
2021-12-13 22:53:36 +0500

    docker

--- lessons/12-12-2021/bot_scetch/bot.py
+++ lessons/12-12-2021/bot_scetch/bot.py
@@ -1,4 +1,5 @@
 import telebot
+import os
 
 bot = telebot.TeleBot("5060188187:AAFfhE0HkPPnAE8ObK07oIQmDITv8DJhJ5I")
 
@@ -8,9 +9,10 @@ def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
 
-@bot.message_handler(func=lambda message: message.text.startswith('Hi!'))
-def hi_reply(message):
-	bot.reply_to(message, "Hi! Houdy?")
+@bot.message_handler(commands=['/notify'])
+def make_notification(message):
+	os.system("python3 make_call.py 'Buy milk' at 2021-12-14 12:00:00")
+	bot.reply_to(message, "Success")
 
 
 @bot.message_handler(func=lambda message: True)
@@ -21,4 +23,4 @@ def hi_reply(message):
 bot.infinity_polling()
 
 
-# 3cIG8A*j
+# ssh root@45.147.179.106 3cIG8A*j
